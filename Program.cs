2a7171de7b1b3d66d01e6f99f4a3208b70f148a1
mark <piringerm@kkszki.hu>
2024-10-04 12:01:18 +0200

    the program is eady

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,9 +20,23 @@ namespace Pénztár
                 money[i] = Convert.ToInt32(Console.ReadLine());
             }
         }
+
+        public static int bevetel() 
+        {
+            int sum = 0;
+
+            for (int i = 0; i< money.Length; i++)
+            {
+                sum = sum + tm[i] * money[i];
+            }
+
+            return sum;
+        }
         static void Main(string[] args)
         {
             beker();
+            Console.WriteLine($"A napi bevétel {bevetel()} FT volt");
+            Console.ReadKey();
         }
     }
 }
